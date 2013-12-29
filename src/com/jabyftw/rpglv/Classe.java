db9b3b20b76ef94561f63027175ce799835f4929
Jabyftw <rfsantos1996@globo.com>
2013-12-28 23:43:13 -0200

    v0.3 - BukkitDev release + dont deal damage/mine if item is proibited

--- src/com/jabyftw/rpglv/Classe.java
+++ src/com/jabyftw/rpglv/Classe.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
--- src/com/jabyftw/rpglv/PlayerListener.java
+++ src/com/jabyftw/rpglv/PlayerListener.java
@@ -7,6 +7,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.inventory.FurnaceExtractEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -24,19 +25,19 @@ import org.bukkit.inventory.AnvilInventory;
  * @author Rafael
  */
 public class PlayerListener implements Listener {
-    
+
     private final RPGLeveling pl;
-    
+
     public PlayerListener(RPGLeveling pl) {
         this.pl = pl;
     }
-    
+
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true) // last when checking player join
     public void onJoin(PlayerJoinEvent e) {
         final Player p = e.getPlayer();
         final String name = p.getName().toLowerCase();
         pl.getServer().getScheduler().scheduleAsyncDelayedTask(pl, new Runnable() {
-            
+
             @Override
             public void run() {
                 Jogador j = pl.sql.getJogador(name);
@@ -46,16 +47,16 @@ public class PlayerListener implements Listener {
             }
         });
     }
-    
+
     @EventHandler
     public void onRespawn(PlayerRespawnEvent e) {
         Player p = e.getPlayer();
         if (pl.players.containsKey(p)) {
             pl.players.get(p).sendStatsToPlayer();
-            
+
         }
     }
-    
+
     @EventHandler
     public void onItemMove(InventoryClickEvent e) {
         if (e.getWhoClicked() instanceof Player) {
@@ -77,7 +78,7 @@ public class PlayerListener implements Listener {
             }
         }
     }
-    
+
     @EventHandler
     public void onPickup(InventoryPickupItemEvent e) {
         if (e.getInventory().getHolder() instanceof Player) {
@@ -92,7 +93,7 @@ public class PlayerListener implements Listener {
             }
         }
     }
-    
+
     @EventHandler
     public void onIteract(PlayerInteractEvent e) {
         Player p = e.getPlayer();
@@ -107,17 +108,17 @@ public class PlayerListener implements Listener {
             }
         }
     }
-    
+
     @EventHandler(ignoreCancelled = false)
     public void onQuit(PlayerQuitEvent e) {
         save(e.getPlayer());
     }
-    
+
     @EventHandler(ignoreCancelled = false)
     public void onKick(PlayerKickEvent e) {
         save(e.getPlayer());
     }
-    
+
     @EventHandler
     public void onEntityDeath(EntityDeathEvent e) {
         Player killer = e.getEntity().getKiller();
@@ -129,26 +130,59 @@ public class PlayerListener implements Listener {
             }
         }
     }
-    
+
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
+        if (e.getDamager() instanceof Player) {
+            Player p = (Player) e.getDamager();
+            if (pl.proibido.contains(p.getItemInHand().getType())) {
+                if (pl.players.containsKey(p)) {
+                    if (!pl.players.get(p).getItemRewardsAllowed().contains(p.getItemInHand().getType())) {
+                        p.sendMessage(pl.getLang("proibitedItem"));
+                        e.setCancelled(true);
+                    }
+                }
+            }
+        }
+    }
+
     @EventHandler(ignoreCancelled = true)
     public void onBreak(BlockBreakEvent e) {
         Player p = e.getPlayer();
         if (pl.players.containsKey(p)) {
+            if (pl.proibido.contains(p.getItemInHand().getType())) {
+                if (pl.players.containsKey(p)) {
+                    if (!pl.players.get(p).getItemRewardsAllowed().contains(p.getItemInHand().getType())) {
+                        p.sendMessage(pl.getLang("proibitedItem"));
+                        e.setCancelled(true);
+                        return;
+                    }
+                }
+            }
             Jogador j = pl.players.get(p);
             j.addExp(j.getClasse().getBreakGain(e.getBlock().getType()));
             e.setExpToDrop(0);
         }
     }
-    
+
     @EventHandler(ignoreCancelled = true)
     public void onPlace(BlockPlaceEvent e) {
         Player p = e.getPlayer();
         if (pl.players.containsKey(p)) {
+            if (pl.proibido.contains(p.getItemInHand().getType())) {
+                if (pl.players.containsKey(p)) {
+                    if (!pl.players.get(p).getItemRewardsAllowed().contains(p.getItemInHand().getType())) {
+                        p.sendMessage(pl.getLang("proibitedItem"));
+                        e.setCancelled(true);
+                        return;
+                    }
+                }
+            }
             Jogador j = pl.players.get(p);
             j.addExp(j.getClasse().getPlaceGain(e.getBlock().getType()));
         }
     }
-    
+
     @EventHandler
     public void onSmelt(FurnaceExtractEvent e) {
         Player p = e.getPlayer();
@@ -158,21 +192,21 @@ public class PlayerListener implements Listener {
             e.setExpToDrop(0);
         }
     }
-    
+
     @EventHandler
     public void onExp(PlayerExpChangeEvent e) {
         if (pl.players.containsKey(e.getPlayer())) {
             e.setAmount(0);
         }
     }
-    
+
     @EventHandler(ignoreCancelled = false, priority = EventPriority.LOWEST)
     public void onEnchant(PrepareItemEnchantEvent e) {
         if (pl.players.containsKey(e.getEnchanter())) {
             e.setCancelled(true);
         }
     }
-    
+
     private void save(Player p) {
         if (pl.players.containsKey(p)) {
             pl.players.get(p).savePlayer();
